713ea49fee8f1d6ff039afdbf47feee7fd7e9505
xislambkrx <140070869+xislambkrx@users.noreply.github.com>
2025-03-20 14:42:28 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(event) {
-    console.log('Service Worker Installed');
-});
-
-self.addEventListener('fetch', function(event) {
-    console.log('Fetching:', event.request.url);
-});
